5b471745d2b87f1993116dd8002c322661f05a7f
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-06 13:23:41 -0400

    Get libedit working.

--- rml.c
+++ rml.c
@@ -30,8 +30,8 @@ char* readline(char* prompt){
 void add_history(char* unused){}
 
 #else
-#include <editline/readline.h>
-#include <editline/history.h>
+//#include <editline/readline.h>
+//#include <editline/history.h>
 #endif
 #include "mpc.h" //Included in projects repo.
 
