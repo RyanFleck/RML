b9530d3c0d67b7e974bfef273c810b39eb983044
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-08 13:52:56 -0500

    Remove includes.

--- rml.c
+++ rml.c
@@ -33,8 +33,8 @@ char* readline(char* prompt){
 void add_history(char* unused){}
 
 #else
-#include <editline/readline.h>
-#include <editline/history.h>
+//#include <editline/readline.h>
+//#include <editline/history.h>
 #endif
 
 
