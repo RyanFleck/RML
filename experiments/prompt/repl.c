9865085f4c47ae5af4887d8d40f0e495c75b3ce2
Ryan Fleck <ryan.fleck@protonmail.com>
2020-01-31 13:35:09 -0500

    Repl experiments.

--- experiments/prompt/repl.c
+++ experiments/prompt/repl.c
@@ -2,10 +2,10 @@
 
 static char input[4096];
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
   puts("RML v0.0.2.0\nPress Ctrl+C to exit");
 
-  while(1){
+  while (1) {
     fputs("rml > ", stdout);
     fgets(input, 4096, stdin);
     printf("Input: %s", input);
